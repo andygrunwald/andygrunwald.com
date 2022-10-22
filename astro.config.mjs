7f8f2a94598fa48ba8583e93cdd4aa3624a2e537
Andy Grunwald <andygrunwald@gmail.com>
2022-10-22 18:48:59 +0200

    Removed "RSS Feed" TODO

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 import image from '@astrojs/image';
 
-// TODO Add RSS Feed
 // TODO Use Image component properly
 
 // https://astro.build/config
