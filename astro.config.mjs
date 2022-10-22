7588aeff2926aae827f3ba662b3ffef91df7866a
Andy Grunwald <andygrunwald@gmail.com>
2022-10-22 19:22:06 +0200

    Remove TODO entry

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 import image from '@astrojs/image';
 
-// TODO Use Image component properly
-
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://andygrunwald.com',
