8edd6eb36ccd276c117f2055d725efd3cec9f425
Andy Grunwald <andygrunwald@gmail.com>
2023-03-12 18:26:18 +0100

    Fix description for RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,7 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 export async function get() {
     return rss({
         title: 'Andy Grunwald (andygrunwald.com)',
-        description: 'I am a Software Engineer, conference and meetup organizer, and open source enthusiast with a passion for on Backend, Infrastructure, Reliability and Engineering Culture.',
+        description: 'Software Engineer and Engineering Manager. Open Source enthusiast with a passion for Backend, Infrastructure, Reliability and Engineering Culture.',
         site: import.meta.env.SITE,
         items: await pagesGlobToRssItems(import.meta.glob(['./blog/*.mdx',])),
         customData: `<language>en-gb</language>`,
