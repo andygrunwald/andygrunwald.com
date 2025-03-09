10d408325421a6cd36cbcf5607326a60d56dfda6
Andy Grunwald <andygrunwald@gmail.com>
2025-03-09 10:10:48 +0100

    WIP: Tailwind upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://andygrunwald.com/',
 	trailingSlash: 'always',
 	integrations: [
-		tailwind(),
 		sitemap(),
 		mdx(),
 	],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
